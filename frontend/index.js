ee027d314fa37f91dbbe4ba6db469eec522f369f
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-01 01:35:49 -0500

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const socket = io('https://limitless-everglades-60126.herokuapp.com/');
+const socket = io('http://localhost:3000/');
 
 socket.on('init', handleInit);
 socket.on('gameState', (msg) => {
