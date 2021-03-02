fde32172e67e525e057a9fe3c81e740253097df1
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-02 11:43:05 -0500

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -581,7 +581,7 @@ var linearGameState = function()
   var right = Object(gameStates[rightIdx]);
   var left = Object(gameStates[rightIdx - 1]);
   var leftFraction = (gameStates[rightIdx].time - displayTime)/(gameStates[rightIdx].time - gameStates[rightIdx-1].time);
-  var rightFraction = 1- leftFraction;
+  var rightFraction = 1 - leftFraction;
 
   out = Object(right);
   for (var i in out.players)
