467caa329e9a2af7872e0b4e0934efadbf772017
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-02 16:33:20 -0500

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -561,7 +561,6 @@ var displayCrosshair = function () {
 var linearGameState = function()
 {
   var displayTime = serverTime() - buffer;
-  console.log(displayTime);
   var rightIdx = 1;
   var time = 0;
   while (rightIdx < gameStates.length && gameStates[rightIdx].time < displayTime)
@@ -575,7 +574,11 @@ var linearGameState = function()
       }
       
   }
-  //console.log(rightIdx, gameStates.length);
+  console.log(rightIdx, gameStates.length);
+  if (rightIdx >= gameStates.length)
+  {
+    rightIdx = gameStates.length - 1;
+  }
   var right = gameStates[rightIdx];
   var left = gameStates[rightIdx - 1];
   var leftFraction = (right.time - displayTime)/(right.time - left.time);
