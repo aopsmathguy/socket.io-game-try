c2da9b6387290fe63195f80439c810a9b1c5b66d
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-02 12:39:42 -0500

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -588,10 +588,10 @@ var linearGameState = function()
      {
         continue;
      }
-     //out.players[i].pos.x = leftFraction * left.players[i].pos.x + rightFraction * right.players[i].pos.x;
-     //out.players[i].pos.y = leftFraction * left.players[i].pos.y + rightFraction * right.players[i].pos.y;
-     out.players[i].pos.x = right.players[i].pos.x;
-     out.players[i].pos.y = right.players[i].pos.y;
+     out.players[i].pos.x = leftFraction * left.players[i].pos.x + rightFraction * right.players[i].pos.x;
+     out.players[i].pos.y = leftFraction * left.players[i].pos.y + rightFraction * right.players[i].pos.y;
+     //out.players[i].pos.x = right.players[i].pos.x;
+     //out.players[i].pos.y = right.players[i].pos.y;
   }
   return out;
 }
