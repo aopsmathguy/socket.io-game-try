46822de16d8f4deddff6e13fb8d00622b8b29129
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-11 13:51:23 -0500

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -163,8 +163,12 @@ var GameState = function (time, players, weapons) {
     if (!targetVel.magnitude() == 0) {
       targetVel = targetVel.multiply(this.players[k].speed / targetVel.magnitude());
     }
-    if (this.players[k].weapon != -1 && this.time - this.weapons[this.players[k].weapon].lastFireTime < 60000 / this.weapons[this.players[k].weapon].firerate) {
-      targetVel = targetVel.multiply(this.weapons[this.players[k].weapon].shootWalkSpeedMult);
+    if (this.players[k].weapon != -1) {
+      targetVel = targetVel.multiply(this.weapons[this.players[k].weapon].walkSpeedMult);
+      if (this.time - this.weapons[this.players[k].weapon].lastFireTime < 60000 / this.weapons[this.players[k].weapon].firerate)
+      {
+        targetVel = targetVel.multiply(this.weapons[this.players[k].weapon].shootWalkSpeedMult);
+      }
     }
     this.players[k].vel = this.players[k].vel.add(targetVel.subtract(this.players[k].vel).multiply(this.players[k].agility));
     this.players[k].ang = controls[k].ang;
@@ -301,17 +305,18 @@ var makeObstacles = function () {
 																], getRandomColor());
 	}
   var viableWeapons = [
-    new Gun(100, 50, 30, false, 780, 1, 15, 1000, 35, 17, 3, 500, 150, 1000, 0.1, 0.12, 0.9, 4, 0.9, 1, '#80f',20,0,20,0),
-    new Gun(100, 50, 35, false, 600, 1, 6, 1300, 35, 33, 8, 500, 150, 1500, 0, 0.12, 0.9, 4, 0.9, 1, '#ff0',20,3,20,3),
+    new Gun(100, 50, 30, false, 780, 1, 15, 1000, 35, 17, 4, 500, 150, 1000, 0.1, 0.12, 0.9, 4, 0.9, 1, 1, '#80f',20,0,20,0),
+    new Gun(100, 50, 35, false, 600, 1, 6, 1500, 42, 33, 8, 750, 200, 1500, 0, 0.12, 0.9, 4, 0.9, 1, 0.6, '#ff0',20,3,20,3),
     
-    new Gun(100, 50, 30, true, 900, 1, 32, 1000, 35, 10, 5, 500, 150, 1000, 0, 0.12, 0.9, 3, 0.9, 0.8, '#f00',20,3,40,6),
-    new Gun(200, 350, 50, true, 600, 1, 30, 1400, 42, 8, 1, 550, 270, 1250, 0, 0.08, 0.91, 3, 0.9, 0.6, '#f80',20,3,45,6),
-    new Gun(200, 50, 60, false, 350, 1, 15, 1500, 50, 25, 3, 710, 200, 2000, 0, 0.3, 0.83, 6, 0.9, 0.3, '#00f',20,3,40,6),
-    new Gun(200, 50, 70, false, 70, 1, 5, 2000, 70, 70, 20, 830, 240, 3000, 0, 0.3, 0.83, 12, 0.9, 0.4, '#8f0',20,3,40,6),
-    new Gun(200, 50, 90, false, 40, 1, 1, 3000, 70, 101, 0, 830, 240, 5000, 0, 0.3, 0.83, 15, 0.9, 0.2, '#000',20,3,40,6),
+    new Gun(100, 50, 30, true, 900, 1, 32, 1200, 35, 10, 5, 500, 150, 1000, 0, 0.12, 0.9, 3, 0.9, 1, 0.8, '#f00',20,3,35,6),
+    new Gun(200, 350, 50, true, 600, 1, 30, 1500, 42, 8, 1, 550, 270, 1500, 0, 0.08, 0.91, 3, 0.9, 0.9, 0.6, '#f80',20,3,40,6),
+    new Gun(200, 50, 60, false, 550, 1, 15, 1600, 50, 25, 3, 710, 200, 2000, 0, 0.3, 0.83, 6, 0.9, 0.8, 0.3, '#00f',20,3,45,6),
     
-    new Gun(200, 220, 35, false, 450, 8, 2, 1250, 30, 13, 9, 350, 56, 700, 0.23, 0, 0.83, 6, 0.9, 0.5, '#f0f',20,3,40,6),
-    new Gun(200, 220, 40, true, 300, 6, 5, 1750, 25, 10, 7, 220, 36, 600, 0.3, 0, 0.83, 6, 0.9, 0.5, '#0ff',20,3,40,6)
+    new Gun(200, 50, 70, false, 70, 1, 5, 2400, 70, 70, 20, 830, 240, 3000, 0, 0.3, 0.83, 12, 0.9, 0.7, 0.6, '#8f0',20,3,45,6),
+    new Gun(200, 50, 90, false, 20, 1, 1, 3000, 200, 101, 0, 830, 240, 5000, 0, 0.3, 0.83, 15, 0.9, 0.5, 0.5, '#000',20,3,50,6),
+    
+    new Gun(200, 220, 35, false, 450, 8, 2, 1250, 30, 13, 9, 350, 56, 700, 0.23, 0, 0.83, 6, 0.9, 1, 0.7, '#f0f',20,3,40,6),
+    new Gun(200, 220, 40, true, 300, 6, 5, 1750, 25, 10, 7, 220, 36, 600, 0.3, 0, 0.83, 6, 0.9, 0.9, 0.5, '#0ff',20,3,40,6)
   ];
   var numEach = [4,2,2,2,2,1,1,2,2];
    var weapons = [];
@@ -337,7 +342,7 @@ function getRandomColor() {
 var Player = function (xStart, yStart, name, id) {
   this.type = "Player";
   setIfUndefined(this, 'speed', 4);
-  setIfUndefined(this, 'agility', .1);
+  setIfUndefined(this, 'agility', 1);
   setIfUndefined(this, 'radius', 20);
   setIfUndefined(this, 'reachDist', 50);
   setIfUndefined(this, 'weapon', -1);
@@ -410,7 +415,7 @@ var Player = function (xStart, yStart, name, id) {
 
 
 }
-var Gun = function (startX, startY, length, auto, firerate, multishot, capacity, reloadTime, bulletSpeed, damage, damageDrop, damageRange, damageDropTension, range, defSpray, sprayCoef, stability, kickAnimation, animationMult, shootWalkSpeedMult, color, handPos1x, handPos1y, handPos2x, handPos2y) {
+var Gun = function (startX, startY, length, auto, firerate, multishot, capacity, reloadTime, bulletSpeed, damage, damageDrop, damageRange, damageDropTension, range, defSpray, sprayCoef, stability, kickAnimation, animationMult, walkSpeedMult, shootWalkSpeedMult, color, handPos1x, handPos1y, handPos2x, handPos2y) {
   this.type = "Gun";
   setIfUndefined(this, 'pos', new Vector(startX, startY));
   setIfUndefined(this, 'vel', new Vector(0, 0));
@@ -434,6 +439,8 @@ var Gun = function (startX, startY, length, auto, firerate, multishot, capacity,
   setIfUndefined(this, 'stability', stability);
   setIfUndefined(this, 'kickAnimation', kickAnimation);
   setIfUndefined(this, 'animationMult', animationMult);
+  
+  setIfUndefined(this, 'walkSpeedMult', walkSpeedMult);
   setIfUndefined(this, 'shootWalkSpeedMult', shootWalkSpeedMult);
 
   setIfUndefined(this, 'color', color);
