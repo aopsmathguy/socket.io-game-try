78bfcc1ad5b1443b90e83aaef2cc5a00822aca95
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-24 01:05:19 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const gameWidth = 3000;
 const gameHeight = 3000;
-const numOb = 200;
-const numHouse1 = 20;
+const numOb = 100;
+const numHouse1 = 40;
 const gridWidth = 400;
 const framesPerTick = 3;
 
