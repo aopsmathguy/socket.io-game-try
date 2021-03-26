85ecf0a11989f90e248cecc271901b94124216e4
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-26 15:30:54 -0400

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -791,7 +791,7 @@ var Gun = function(name, startX, startY, length, auto, firerate, multishot, capa
                 var stretch = this.radius - dist;
                 if (stretch > 0)
                 {
-                    finalForce = finalForce.add((new Vector(0.1*stretch,0)).rotate(this.pos.angTo(closestPoint)));
+                    finalForce = finalForce.add((new Vector(stretch,0)).rotate(this.pos.angTo(closestPoint)));
                 }
             }
         });
