6302d1cb67f30a431d0adcb72abeccdd8d977496
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-27 02:01:04 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -913,7 +913,7 @@ var linearGameState = function() {
 
     var right = gameStates[rightIdx];
     var left = gameStates[rightIdx - 1];
-
+    console.log(1000/(right.time - left.time));
     var out = JSON.parse(JSON.stringify(right));
     giveMethods(out);
     out.time = displayTime;
@@ -978,6 +978,7 @@ function updateGameArea() {
         }
         state.render();
         emitMousePos();
+        
     }
 }
 
