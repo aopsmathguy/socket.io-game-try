cdb13e3f4a86895ed05a607786e940ad10e83f8f
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-28 18:46:07 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -793,7 +793,7 @@ var Gun = function(name, startX, startY, length, auto, firerate, multishot, capa
             var stretch = this.radius + weapon.radius - dist;
             if (stretch > 0)
             {
-                finalForce = finalForce.add((new Vector(0.1*stretch,0)).rotate(this.pos.angTo(weapon.pos)));
+                finalForce = finalForce.add((new Vector(0.03*stretch,0)).rotate(this.pos.angTo(weapon.pos)));
             }
         });
         loopThroughObstacles(this.pos, (obstacle) => {
@@ -807,7 +807,7 @@ var Gun = function(name, startX, startY, length, auto, firerate, multishot, capa
             var stretch = this.radius - dist;
             if (stretch > 0)
             {
-                finalForce = finalForce.add((new Vector(0.2*stretch,0)).rotate(this.pos.angTo(closestPoint)));
+                finalForce = finalForce.add((new Vector(0.06*stretch,0)).rotate(this.pos.angTo(closestPoint)));
             }
             
         });
