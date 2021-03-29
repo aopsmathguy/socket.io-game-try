f52241a0440b6617e4950050bdb7cb0d820be35f
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-29 02:07:54 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -980,10 +980,13 @@ var linearGameState = function() {
             {
                 bullet.hitPoint = -1;
             }
-            bullet.objectsIntersection(out);
-            if (bullet.hitPoint != -1 && bullet.startPos.distanceTo(bullet.hitPoint) <= bullet.startPos.distanceTo(bullet.tailPos) || bullet.startPos.distanceTo(rightBull.pos) < framesPerTick * bullet.vel.magnitude() * (right.time - displayTime) / (right.time - left.time))
+            else
             {
-                //delete out.weapons[i].bullets[j];
+                bullet.objectsIntersection(out);
+            }
+            if (bullet.hitPoint != -1 && bullet.startPos.distanceTo(bullet.hitPoint) > bullet.startPos.distanceTo(bullet.tailPos) || bullet.startPos.distanceTo(rightBull.pos) < framesPerTick * bullet.vel.magnitude() * (right.time - displayTime) / (right.time - left.time))
+            {
+                delete out.weapons[i].bullets[j];
             }
         }
         out.weapons[i].recoil = linearPosition(new Vector(left.weapons[i].recoil, 0), new Vector(right.weapons[i].recoil, 0), displayTime, left.time, right.time).x;
