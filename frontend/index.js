1b0469383b92298b862d5566dce3e36fb5eab8e6
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-03-29 01:19:26 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -931,7 +931,6 @@ var linearGameState = function() {
         }
         out.weapons[i].pos = linearPosition(left.weapons[i].pos,right.weapons[i].pos, displayTime, left.time,right.time);
         var arrIdx = arrayUnique(Object.keys(right.weapons[i].bullets).concat(Object.keys(left.weapons[i].bullets)));
-        console.log(arrIdx);
         for (var j in arrIdx) {
             var rightBull;
             var leftBull;
