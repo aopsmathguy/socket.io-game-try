eab7ba10962adc7ca2a7f1d644863322a4205001
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-12 08:14:20 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const numOb = 30;
 const numHouse1 = 10;
 const numHouse2 = 10;
 const gridWidth = 250;
-const framesPerTick = 10;
+const framesPerTick = 3;
 
 const io = require('socket.io')();
 
@@ -162,12 +162,12 @@ function emitNewMessage(message) {
 function emitGameState(gameState) {
     // Send this event to everyone in the room.
     var copy = JSON.parse(JSON.stringify(gameState));
-    logTime("copy",()=>{
+    //logTime("copy",()=>{
         copy = trimObject(copy);
-    });
-    logTime("emitcopy",()=>{
+    //});
+    //logTime("emitcopy",()=>{
         io.sockets.emit('gameState', copy);
-    });
+    //});
 }
 var setIfUndefined = function(obj, field, value) {
     if (obj[field] === undefined) {
@@ -1320,12 +1320,12 @@ makeObstacles();
 var stage = 0;
 setInterval(updateGameArea, 1000 / 60);
 setInterval(() => {
-    logTime("push",()=>{
+    //logTime("push",()=>{
         for (var i in gameState.weapons)
         {
           gameState.weapons[i].pushFromAll(gameState);
         }
-    });
+    //});
 },1000);
 function updateGameArea() {
     //logTime("updateGameArea", () => {
