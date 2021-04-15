6f29fa3d53b068aae63c9fbd95734fb0b05b5d57
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-14 23:41:51 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -797,8 +797,8 @@ var Bullet = function() {
         var ctx = myGameArea.context;
         const g = drawer.createLinearGradient(ctx, this.pos, this.pos.add((new Vector(-this.trailLength, 0)).rotate(this.ang)));
         g.addColorStop(0, hexToRgbA("#ff0", 1)); // opaque
-        g.addColorStop(0.1/3, hexToRgbA("#ff0", 1)); // opaque
-        g.addColorStop(0.2/3, hexToRgbA('#ccc', 0.25)); // opaque
+        g.addColorStop(0.07/3, hexToRgbA("#ff0", 1)); // opaque
+        g.addColorStop(0.14/3, hexToRgbA('#ccc', 0.25)); // opaque
         g.addColorStop(1/3, hexToRgbA('#ccc', 0)); // transparent
         ctx.strokeStyle = g;
 
