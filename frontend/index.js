56e2f8bb4da165e2c882a908e67c3448d6497c0d
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-19 23:39:30 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -137,7 +137,7 @@ var yourKillFeed = {
             ctx.save();
             ctx.globalAlpha = txtAlpha;
             ctx.font = "bold 20px Courier New";
-            ctx.strokeWidth = 6;
+            ctx.lineWidth = 6;
             ctx.strokeStyle = "black";
             ctx.fillStyle = "white";
             ctx.textAlign = "center";
