c1519a7502ab0e6d08cb101284463e95d0f0a739
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-22 13:30:25 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1218,7 +1218,7 @@ var displayCrosshair = function() {
     controlsBundle.mouse.circle(6, '#fff', 2);
 }
 var linearInterpolator = {
-    lagLimit : 150,
+    lagLimit : 4000,
     linearValue : function(v1, v2, t, t1, t2) {
         var ratio = Math.max(-this.lagLimit, t2 - t) / (t2 - t1);
         return v1 * ratio + v2 * (1-ratio);
