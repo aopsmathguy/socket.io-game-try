c2231c2aa64d06e3a387663bec18c6024aae4461
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-22 10:23:22 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1347,7 +1347,7 @@ var linearInterpolator = {
                 {
                     if (bullet.hitPoint != -1 && bullet.startPos.distanceTo(bullet.hitPoint) < bullet.startPos.distanceTo(bullet.tailPos) || bullet.startPos.distanceTo(rightBull.pos) < framesPerTick * bullet.vel.magnitude() * (right.time - displayTime) / (right.time - left.time))
                     {
-                        delete out.weapons[i].bullets[j];
+                        //delete out.weapons[i].bullets[j];
                     }
                     else
                     {
