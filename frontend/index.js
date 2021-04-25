70870df565f96e536e9065180f98985017d18fb3
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-04-25 15:46:53 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -429,8 +429,7 @@ var myGameArea = {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
 
-        this.context.fillStyle = "#34a318";
-        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     }
 }
