8d35bc0102bd9059e46ef4dfb94fdc929c64d7c0
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-05-04 18:58:39 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -428,11 +428,13 @@ var myGameArea = {
     },
     clear: function() {
         var scale = 2;
+        
+        
+        this.canvas.width = window.innerWidth * scale;
+        this.canvas.height = window.innerHeight * scale;
+        
         this.canvas.style.width = window.innerWidth;
         this.canvas.style.height = window.innerHeight;
-        
-        this.canvas.width = this.canvas.style.width * scale;
-        this.canvas.height = this.canvas.style.height * scale;
         
         this.uiScale = (this.canvas.width + this.canvas.height)/2000;
         this.uiWidth = this.canvas.width/this.uiScale;
