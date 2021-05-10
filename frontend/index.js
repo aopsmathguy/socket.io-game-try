f413c37acc5f13d4749bff2c38d1d52e3cbc306e
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-05-10 13:17:25 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -527,7 +527,7 @@ var GameState = function() {
     this.render = function() {
         myGameArea.transformUi(() => {
             drawer.transformPoint(() => {
-                this.displayGrid(100);
+                this.displayGrid(500);
                 loopThroughDisplayObstacles(drawer.scroll, (obstacle) => {
                     if (!obstacle.intersectable) {
                         obstacle.display();
@@ -587,7 +587,7 @@ var GameState = function() {
         var endY = delta*(Math.floor((drawer.scroll.y + height/2)/delta) + 1);
         var ctx = myGameArea.context;
         ctx.lineWidth = 2;
-        ctx.strokeStyle = "rgba(0,0,0,0.2)";
+        ctx.strokeStyle = "rgba(0,0,0,0.3)";
         for (var x = startX; x <= endX; x += delta)
         {
             ctx.beginPath();
