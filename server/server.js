9fa85b80717f51d94c9a8163693c0dfb85efac87
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-07-13 16:24:15 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -358,7 +358,7 @@ const constants = {
         this.structures = [
             new Structure(
               "rock",
-              30,
+              45,
               new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'#000',false),
               [],
               [
@@ -369,13 +369,13 @@ const constants = {
             ),
             new Structure(
               "bush",
-              30,
+              10,
               new Obstacle([(new Vector(10,0)).rotate(0),(new Vector(10,0)).rotate(Math.PI/3),(new Vector(10,0)).rotate(2 * Math.PI/3),(new Vector(10,0)).rotate(3 * Math.PI/3),(new Vector(10,0)).rotate(4 * Math.PI/3),(new Vector(10,0)).rotate(5 * Math.PI/3)],'#000',false),
+              [],
               [],
               [
                 new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'rgba(77, 117, 57, 0.9)',false)
               ],
-              [],
               [0,Math.PI/2,Math.PI,3*Math.PI/2],2.5,3.5
             ),
             new Structure(
