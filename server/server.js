d95051181097ad91cf1b3cd355a756fefe5daffd
aopsmathguy <40621169+aopsmathguy@users.noreply.github.com>
2021-07-14 16:39:29 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -365,7 +365,7 @@ const constants = {
         this.structures = [
             new Structure(
               "rock",
-              20,
+              15,
               new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'#000',false),
               [],
               [
@@ -376,25 +376,25 @@ const constants = {
             ),
             new Structure(
               "bush",
-              10,
+              8,
               new Obstacle([(new Vector(10,0)).rotate(0),(new Vector(10,0)).rotate(Math.PI/3),(new Vector(10,0)).rotate(2 * Math.PI/3),(new Vector(10,0)).rotate(3 * Math.PI/3),(new Vector(10,0)).rotate(4 * Math.PI/3),(new Vector(10,0)).rotate(5 * Math.PI/3)],'#000',false),
               [],
               [],
               [
-                new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'rgba(77, 117, 57, 0.97)',false)
+                new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'rgba(77, 117, 57, 0.95)',false)
               ],
               [0,Math.PI/2,Math.PI,3*Math.PI/2],2.5,3.5
             ),
             new Structure(
               "tree",
-              40,
+              30,
               new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'#000',false),
               [],
               [
                   new Obstacle([(new Vector(20,0)).rotate(0),(new Vector(20,0)).rotate(Math.PI/3),(new Vector(20,0)).rotate(2 * Math.PI/3),(new Vector(20,0)).rotate(3 * Math.PI/3),(new Vector(20,0)).rotate(4 * Math.PI/3),(new Vector(20,0)).rotate(5 * Math.PI/3)],'#654321',true)
               ],
               [
-                new Obstacle([(new Vector(70,0)).rotate(0),(new Vector(70,0)).rotate(Math.PI/3),(new Vector(70,0)).rotate(2 * Math.PI/3),(new Vector(70,0)).rotate(3 * Math.PI/3),(new Vector(70,0)).rotate(4 * Math.PI/3),(new Vector(70,0)).rotate(5 * Math.PI/3)],'rgba(77, 117, 57, 0.93)',false)
+                new Obstacle([(new Vector(70,0)).rotate(0),(new Vector(70,0)).rotate(Math.PI/3),(new Vector(70,0)).rotate(2 * Math.PI/3),(new Vector(70,0)).rotate(3 * Math.PI/3),(new Vector(70,0)).rotate(4 * Math.PI/3),(new Vector(70,0)).rotate(5 * Math.PI/3)],'rgba(77, 117, 57, 0.85)',false)
               ],
               [0,Math.PI/2,Math.PI,3*Math.PI/2],1.5,2
             ),
